f694f7f7e5bc4e5415b12cdcd2bca7905546fe01
Hans <741581879@qq.com>
2020-07-28 22:15:01 +0800

    [Story2][Hans] feature: Add Emplyee class

--- src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
@@ -1,7 +1,22 @@
 package com.thoughtworks.springbootemployee.model;
 
 public class Employee {
-    private int companyId;
+    private int id;
+    private String gender;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
