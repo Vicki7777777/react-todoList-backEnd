09555ae46a47e68268449a483fa948b4766f7d9c
Hans <741581879@qq.com>
2020-07-28 23:01:01 +0800

    [Story1][Hans] feature: Add get companys by page

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -14,8 +14,26 @@ public class CompanyController {
     List<Company> companies = new LinkedList<>();
 
     @GetMapping
-    public List<Company> getCompanies() {
-        return companies;
+    public List<Company> getCompanies(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "0") int pageSize
+    ) {
+        if (page == 0) {
+            return companies;
+        } else {
+            int begin;
+            int end;
+            int count = companies.size();
+
+            begin = (page - 1) * pageSize + 1;
+            if (count - begin > pageSize) {
+                end = begin + pageSize - 1;
+            } else {
+                end = count - 1;
+            }
+
+            return companies.subList(begin, end);
+        }
     }
 
     @PostMapping
@@ -26,8 +44,8 @@ public class CompanyController {
     @GetMapping("/{id}")
     public Company getCompany(@PathVariable int id) {
         Company company = null;
-        for(Company companyItem : companies) {
-            if(companyItem.getId() == id) {
+        for (Company companyItem : companies) {
+            if (companyItem.getId() == id) {
                 company = companyItem;
             }
         }
@@ -37,8 +55,8 @@ public class CompanyController {
     @GetMapping("/{id}/employees")
     public List<Employee> getAllEmployees(@PathVariable int id) {
         List<Employee> employees = null;
-        for(Company company : companies) {
-            if(company.getId() == id) {
+        for (Company company : companies) {
+            if (company.getId() == id) {
                 employees = company.getEmployees();
             }
         }
@@ -47,8 +65,8 @@ public class CompanyController {
 
     @PutMapping("/{id}")
     public void updateCompany(@PathVariable int id, @RequestBody Company company) {
-        for(Company companyItem : companies) {
-            if(companyItem.getId() == id) {
+        for (Company companyItem : companies) {
+            if (companyItem.getId() == id) {
                 companyItem.setEmployees(company.getEmployees());
                 companyItem.setId(company.getId());
             }
@@ -57,8 +75,8 @@ public class CompanyController {
 
     @DeleteMapping("/{id}")
     public void deleteAllEmployees(@PathVariable int id) {
-        for(Company companyItem : companies) {
-            if(companyItem.getId() == id) {
+        for (Company companyItem : companies) {
+            if (companyItem.getId() == id) {
                 companyItem.getEmployees().clear();
             }
         }
