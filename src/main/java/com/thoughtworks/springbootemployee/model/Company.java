e96b4c8c965175fda2519bae1a91410136cacd19
Hans <741581879@qq.com>
2020-07-28 18:13:24 +0800

    [Story1][Hans] feature: Add Company class

---
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.springbootemployee.model;
+
+public class Company {
+    private int id;
+    private String name;
+
+    public Company(String name, int id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
